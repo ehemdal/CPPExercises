798f0ac4c6f76ede1f813c36f7ab509aa537a448
ehemdal <erik.hemdal@gmail.com>
2021-02-21 14:05:33 -0500

    Adding a program demonstrating exercise 4.12.

---
+++ ch4Exercises/ex4-12/Mystery.cpp
@@ -0,0 +1,19 @@
+// ex4-12.cpp : Mystery -- what does this program do?
+// Try to work it out before you run the program.
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    unsigned int x{ 1 };
+    unsigned int total{ 0 };
+    while (x <= 10) {
+        int y = x * x;
+        cout << y << endl;
+        total += y;
+        ++x;
+    }
+    cout << "Total is: " << total << endl;
+}
+
