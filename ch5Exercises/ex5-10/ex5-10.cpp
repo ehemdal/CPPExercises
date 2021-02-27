a551c234644e3b5f3f688e92660ea14aabf49015
ehemdal <erik.hemdal@gmail.com>
2021-02-27 13:31:15 -0500

    Adding exercise 5.10 example

---
+++ ch5Exercises/ex5-10/ex5-10.cpp
@@ -0,0 +1,13 @@
+// ex5-10.cpp : What does this print?
+#include <iostream>
+using namespace std;
+
+int main() {
+    for (int i{ 1 }; i <= 10; i++) {
+        for (int j{ 1 }; j <= 5; j++) {
+            cout << '@';
+        }
+
+        cout << endl;
+    }
+}
