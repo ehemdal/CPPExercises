93f14a408f189f5112d2d121882c4bccd70c1328
ehemdal <erik.hemdal@gmail.com>
2021-02-28 15:19:17 -0500

    Updated exercise 5.13 to make it easier to compare the textbook version to my version.

--- ch5Exercises/ex5-13/ex5-13.cpp
+++ ch5Exercises/ex5-13/ex5-13.cpp
@@ -1,10 +1,17 @@
 // ex5-13.cpp : Factorials example
-// What's wrong with using data type long as the textbook suggests?
+// What's wrong with using data type long as the textbook suggests?  Define TEXT_VERSION to find out
 // Hint:  see https://docs.microsoft.com/en-us/cpp/cpp/data-type-ranges?view=msvc-160
+
+//#define TEXT_VERSION
 
 #include <iostream>
 using namespace std;
+
+#ifdef TEXT_VERSION
+long fact{ 1 };
+#else
 long long fact{ 1 };  // compare with type long
+#endif
 
 /* Calculate and display 1! through 20! */
 int main()
