1c065869b7c9654c35f866b34bbc1d705533c76f
ehemdal <erik.hemdal@gmail.com>
2021-03-05 11:38:50 -0500

    Adding exercise 6.18.

---
+++ ch6Exercises/ex6-18/ex6-18.cpp
@@ -0,0 +1,27 @@
+// ex6-18.cpp : Exponentiation
+//
+
+#include <iostream>
+
+int integerPower(int, int);
+
+int main()
+{
+    std::cout << integerPower(3, 4);
+}
+/*
+    exponent is a positive non-zero integer
+    base is an integer
+    return base raised to the power of exponent without using math library functions.
+    Recursion seems the simplest way to write this.
+*/
+int integerPower(int base, int exponent) {
+    
+    if (1 == exponent) {
+        return base;
+    }
+    else {
+        return base * integerPower(base, (exponent - 1));
+    }
+
+}
