1ee0de605a05e374c5939d89c61fedf1f4aacc36
ehemdal <erik.hemdal@gmail.com>
2021-03-05 11:52:41 -0500

    Adding 6.19

---
+++ ch6Exercises/ex6-19/ex6-19.cpp
@@ -0,0 +1,20 @@
+// ex6-19.cpp : Find the hypotenuse of a right triangle given the opposite two sides.
+//
+
+#include <iostream>
+#include <math.h>
+
+double hypotenuse(double, double);
+
+int main()
+{
+    std::cout << "Triangle 1: " << hypotenuse(3.0, 4.0) << std::endl;
+    std::cout << "Triangle 2: " << hypotenuse(5.0, 12.0) << std::endl;
+    std::cout << "Triangle 3: " << hypotenuse(8.0, 15.0) << std::endl;
+}
+
+double hypotenuse(double a, double b) {
+    // Use the Pythagorean theorem to find the hypotenuse
+
+    return (sqrt((a * a) + (b * b)));
+}
