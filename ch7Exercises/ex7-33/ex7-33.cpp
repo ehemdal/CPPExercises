67e250531ba0876f5f1a3896681ab3d663bf3230
ehemdal <erik.hemdal@gmail.com>
2021-03-14 16:58:09 -0400

    WIP exercise 7.33; cleaning up the array and sorting the array semantics.

--- ch7Exercises/ex7-33/ex7-33.cpp
+++ ch7Exercises/ex7-33/ex7-33.cpp
@@ -9,26 +9,30 @@ using namespace std;
 
 const size_t columns{ 6 };
 const size_t rows{ 6 };
-void printArray(const array<array<string, columns>, rows>&);
+void printArray(const array<array<char, columns>, rows>&);
 
 int main()
 {
 
  
-array<array<string, columns>, rows> maze = {  "X", "X", "X", "X", "X", "X",
-                                            "X", "X", "X", "X", "X", "X",
-                                            "X", "X", "X", "X", "X", "X", 
-                                            "X", "X", "X", "X", "X", "X",
-                                            "X", "X", "X", "X", "X", "X",
-                                            "X", "X", "X", "X", "X", "X", };
+array<array<char, columns>, rows> maze = {  '#', '#', '#', '#', '#', '#',
+                                              '#', '.', '.', '.', '#', '#',
+                                              '.', '.', '#', '.', '#', '#', 
+                                              '#', '#', '#', '.', '#', '#',
+                                              '#', '#', '#', '.', '.', '.',
+                                              '#', '#', '#', '#', '#', '#' };
 printArray(maze);
 
 }
-void printArray(const array<std::array<string, columns>, rows>& a) {
-
+void printArray(const array<std::array<char, columns>, rows>& a) {
+    // rows and columns start at the upper left
+    // RC0 C1 C2 ...
+    // R1
+    // R2
+    // ...
     for (auto const& row : a) {
         for (auto const& element : row) {
-            std::cout << element;
+            std::cout << element << ' ';
         }
         std::cout << endl;
     }
