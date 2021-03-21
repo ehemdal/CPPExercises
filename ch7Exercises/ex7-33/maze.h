bb4a0ca64ee3d61379c664dbbd72f0b2c2ccdc63
ehemdal <erik.hemdal@gmail.com>
2021-03-21 16:53:37 -0400

    Testing this exercise.

--- ch7Exercises/ex7-33/maze.h
+++ ch7Exercises/ex7-33/maze.h
@@ -5,8 +5,8 @@
 class Maze {
 public:
     //Public members
-    static const int rows{ 6 };
-    static const int cols{ 6 };
+    static const int rows{ 12 };
+    static const int cols{ 12 };
     enum Direction { UP, DOWN, LEFT, RIGHT };
 
     // Constructor
@@ -15,7 +15,34 @@ public:
         std::cout << "calling Maze constructor" << std::endl;
     }
 
+    void mazeTraverse(int rowpos, int colpos) {
 
+        //Mark the current position
+        MyMaze[rowpos][colpos] = 'X';
+
+        //display the maze
+        printMaze();
+
+        //Return if done
+        if (colpos == (MyMaze[rowpos].size() - 1)) return;
+
+        //where can we move?//
+        if (MyMaze[rowpos][colpos + 1] == '.') { // go straight
+            mazeTraverse(rowpos, (colpos + 1));
+        }
+        else if (MyMaze[rowpos + 1][colpos] == '.') {// can't go straight, so try turning right.
+            mazeTraverse((rowpos + 1), colpos);
+        }
+        else if (MyMaze[rowpos - 1][colpos] == '.') { // can't go right, so try turning left
+            mazeTraverse((rowpos - 1), colpos);
+        }
+        else { //dead end, so go back a step
+            mazeTraverse(rowpos, (colpos - 1));
+        }
+
+
+
+    };
 
     void printMaze() {
         for (int i = 0; i < cols; i++)
@@ -29,7 +56,7 @@ public:
     };
 
 private:
-    std::array<std::array<char, 6>, 6> MyMaze; // 2D array
+    std::array<std::array<char, 12>, 12> MyMaze; // 2D array
     //position variables
     int c{ 0 }; //column position
     int r{ 0 }; //row position
