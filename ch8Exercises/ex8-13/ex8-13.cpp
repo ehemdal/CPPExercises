7107edc298e09cfd9e9307720e69b25d2acb2892
ehemdal <erik.hemdal@gmail.com>
2021-03-29 14:57:02 -0400

    Adding exercise 8.13

---
+++ ch8Exercises/ex8-13/ex8-13.cpp
@@ -0,0 +1,30 @@
+// ex8-13.cpp : What does this code do?
+//
+
+#include <iostream>
+using namespace std;
+
+void mystery1(char*, const char*);
+
+
+int main()
+{
+    char string1[80];
+    char string2[80];
+
+    cout << "Enter two strings: ";
+    cin >> string1 >> string2;
+    mystery1(string1, string2);
+    cout << string1 << endl;
+}
+
+void mystery1(char* s1, const char* s2) {
+    while (*s1 != '\0') {
+        ++s1;
+    }
+
+    for (; (*s1 = *s2); ++s1, ++s2) {
+        ;
+    }
+
+}
