d65b4ddc6a05dbc5e7ec66f635ab0fd5d4e3e9ea
ehemdal <erik.hemdal@gmail.com>
2021-04-03 14:31:02 -0400

    Added basic Simpletron execution loop.

--- ch8Exercises/Simpletron/Simpletron.cpp
+++ ch8Exercises/Simpletron/Simpletron.cpp
@@ -8,5 +8,70 @@ int main()
 {
     Welcome();
     loadProgram(memory);
+    while (halt != operationCode && instructionCounter < MEMORY_SIZE) {
+
+        //Fetch the next instruction
+        instructionRegister = memory[instructionCounter];
+        operationCode = instructionRegister / 100;
+        operand = instructionRegister % 100;
+        instructionCounter++;
+
+        // Decode the instruction
+        switch (operationCode) 
+        {
+        case read:
+            std::cin >> memory[operand];
+            break;
+
+        case write:
+            std::cout << memory[operand];
+            break;
+
+        case load:
+            accumulator = memory[operand];
+            break;
+
+        case store:
+            memory[operand] = accumulator;
+            break;
+        
+        case add:
+            accumulator += memory[operand];
+            break;
+        
+        case subtract:
+            accumulator -= memory[operand];
+            break;
+        
+        case divide:
+            accumulator /= memory[operand];
+            break;
+        
+        case multiply:
+            accumulator *= memory[operand];
+            break;
+        
+        case branch:
+            instructionCounter = operand;
+            break;
+        
+        case branchneg:
+            if (0 == accumulator) instructionCounter = operand;
+            break;
+        
+        case branchzero:
+            if (accumulator < 0) instructionCounter = operand;
+            break;
+        
+        case halt:
+            std::cout << "*** Simpletron execution terminated ***" << std::endl;
+        
+        default:
+            dumpMemory(memory);
+        }
+    }
+
+
+
     dumpMemory(memory);
 }
