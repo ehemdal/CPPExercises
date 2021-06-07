3d195c9b1657a5e5b7b8345efd5927337c18709b
ehemdal <erik.hemdal@gmail.com>
2021-06-07 15:27:36 -0400

    Added a better reference for why the 3-way operator is useful.

--- CourseExamples/CPP20_3way/CPP20_3way.cpp
+++ CourseExamples/CPP20_3way/CPP20_3way.cpp
@@ -1,11 +1,14 @@
 // CPP203way.cpp : Test the new 3-way "spaceship" operator.  This program was corrected and adapted from 
 // https://www.geeksforgeeks.org/features-of-c-20/ and see here
 // https://en.cppreference.com/w/cpp/language/default_comparisons
+// https://devblogs.microsoft.com/cppblog/simplify-your-code-with-rocket-science-c20s-spaceship-operator/#:~:text=The%20spaceship%20operator%20is%20a%20welcomed%20addition%20to,right%20now%20in%20Visual%20Studio%202019%20under%20%2Fstd%3Ac%2B%2Blatest%21
+// 
 //
 
 #include <compare>
 #include <iostream>
 #include <vector>
+#include <typeinfo>
 using namespace std;
 
 // Driver Code
