edb13adb0c76845c49e13762bb8e464aed996ef8
ehemdal <erik.hemdal@gmail.com>
2022-01-11 16:51:48 -0500

    Retargeting projects

--- ch10Exercises/Fig10_13/Fig10_13.cpp
+++ ch10Exercises/Fig10_13/Fig10_13.cpp
@@ -9,7 +9,7 @@ void outputArray(const Array &); // prototype
 int main() {
    Array integers1(7); // 7-element Array
    outputArray(integers1); // output Array integers1
-   outputArray(3); // convert 3 to an Array and output Array’s contents
+
    outputArray(Array(3)); // explicit single-argument constructor call
 }  // end main
 
