6278952b3258919b38bfd290fd4b738724a3fe67
ehemdal <erik.hemdal@gmail.com>
2022-06-05 10:07:30 -0400

    Adding a simple iterator example needed for a student question.

---
+++ CourseExamples/SimpleIterator/SimpleIterator.cpp
@@ -0,0 +1,61 @@
+// C++ program to demonstrate iterators
+// Adapted from https://www.geeksforgeeks.org/difference-between-iterators-and-pointers-in-c-c-with-examples/
+// for RSEG-0103 discussion
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+	// Declaring a vector
+	vector<int> v = { 1, 2, 3 };
+
+	// Declaring an iterator
+	vector<int>::iterator i;
+
+	int j;
+
+	cout << "Without iterators = ";
+
+	// Accessing the elements without using iterators
+	// Notice the loop bound of 3
+	for (j = 0; j < 3; ++j) {
+		cout << v[j] << " ";
+	}
+
+	cout << "\nWith iterators = ";
+
+	// Accessing the elements using iterators
+	for (i = v.begin(); i != v.end(); ++i) {
+		cout << *i << " ";
+	}
+
+	// Accessing the elements without using iterators -- 
+	// You need to know the length of the vector for this to work.
+	/*
+	for (j = 0; j < 4; ++j) {
+		cout << v[j] << " ";
+	}
+	*/
+
+	// Adding one more element to vector
+	v.push_back(4);
+
+	cout << "\nWithout iterators = ";
+
+	// Accessing the elements without using iterators
+	// Notice that the loop bound has been changed from what is in line 23
+	for (j = 0; j < 4; ++j) {
+		cout << v[j] << " ";
+	}
+
+	cout << "\nWith iterators = ";
+
+	// Accessing the elements using iterators
+	for (i = v.begin(); i != v.end(); ++i) {
+		cout << *i << " ";
+	}
+
+	return 0;
+}
