281dd5cf4276966b0a72113783ffc8bf53ea0e29
ehemdal <erik.hemdal@gmail.com>
2025-02-09 14:19:21 -0500

    Added acknowledgement and some fancier output. Added an overload for operator+.

--- CourseExamples/wk5Array/ArrayOverload.cpp
+++ CourseExamples/wk5Array/ArrayOverload.cpp
@@ -1,19 +1,23 @@
-// Examples of operator overloading so we can use <<, =, ==, and != on arrays.
+// Examples of operator overloading so we can use  +, <<, =, ==, and != on arrays.
+// Adapted from chapter 10 case study in 
+// Deitel & Deitel, C++ How to Program 10th ed (2017: Pearson Education).
+
 #include <iostream>
 #include <cstdlib>
+#include <typeinfo>
 
 using namespace std;
 
-class Array {
+class IntArray {
 // Private data: pointer to array memory and integer size
 private:
     int* m_ptr;
     int m_size;
 
 public:
-    Array(int* ptr, int size);  // A
-    Array(int size); // B
-    Array(const Array&); // C
+    IntArray(int* ptr, int size);  // A
+    IntArray(int size); // B
+    IntArray(const IntArray&); // C
 
     // Overloading [] (subscript) operator, will be used as  arr[5] = 25;
     int& operator[] (int); // D
@@ -22,29 +26,31 @@ public:
     int operator [] (int) const; // E
     
     // overloading copy assignment operator
-    Array& operator=(const Array& other); // F
+    IntArray& operator=(const IntArray& other); // F
 
     // Pre- and post-increment operators
-    Array& operator ++(); // pre-increment // J
-    Array operator ++(int); // post-increment // K
-    Array& operator --(); // pre-decrement  // L
-    Array operator --(int); // post-decrement // M
+    IntArray& operator ++(); // pre-increment // J
+    IntArray operator ++(int); // post-increment // K
+    IntArray& operator --(); // pre-decrement  // L
+    IntArray operator --(int); // post-decrement // M
+
+    IntArray operator +(const IntArray& addend); // N
 
 
     /*
       overloading << operator. We have to declare it as friend to access 
       private members m_ptr and m_size
     */
-    friend std::ostream& operator<<(std::ostream&, const Array&);  // G
+    friend std::ostream& operator<<(std::ostream&, const IntArray&);  // G
 
-    friend bool operator==(const Array& lhs, const Array& rhs); // H
-    friend bool operator!=(const Array& lhs, const Array& rhs); // I
+    friend bool operator==(const IntArray& lhs, const IntArray& rhs); // H
+    friend bool operator!=(const IntArray& lhs, const IntArray& rhs); // I
 };
 
 // A: This constructor takes a size and a pointer to another array.
 // It creates a new array of size size and copies from the other array
 // What happens if the other array is smaller than the new one?
-Array::Array(int* ptr = nullptr, int size = 0) //JEH fix, use nullptr
+IntArray::IntArray(int* ptr = nullptr, int size = 0) //JEH fix, use nullptr
 {
     m_size = size;
     m_ptr = nullptr;
@@ -57,7 +63,7 @@ Array::Array(int* ptr = nullptr, int size = 0) //JEH fix, use nullptr
 }
 
 // B: Construct an array with all elements set to zero
-Array::Array(int size)
+IntArray::IntArray(int size)
 {
     m_size = size;
     m_ptr = nullptr;
@@ -70,7 +76,7 @@ Array::Array(int size)
 }
 
 // C: Copy constructor.  Make a new array using a reference to an existing one
-Array::Array(const Array& other)
+IntArray::IntArray(const IntArray& other)
 {
     m_ptr = new int[other.m_size];
     m_size = other.m_size;
@@ -80,7 +86,7 @@ Array::Array(const Array& other)
 }
 
 // D: [] operator: Return an array element at index as a modifiable value
-int& Array::operator[](int index)
+int& IntArray::operator[](int index)
 {
     if (index >= m_size)
     {
@@ -90,7 +96,7 @@ int& Array::operator[](int index)
 }
 
 // E: Return an array element as const (non-modifiable value)
-int Array::operator [] (int index) const {
+int IntArray::operator [] (int index) const {
     if (index >= m_size)
     {
         throw std::invalid_argument("Index value is greater than array size");
@@ -99,7 +105,7 @@ int Array::operator [] (int index) const {
 }
 
 // F: Overloaded assignment operator
-Array& Array::operator=(const Array& other) {
+IntArray& IntArray::operator=(const IntArray& other) {
     if (this != &other) {
         if (other.m_size != m_size) {
             delete[] m_ptr;
@@ -118,8 +124,9 @@ Array& Array::operator=(const Array& other) {
 }
 
 // G: Ostream (output stream) operator for an array
-std::ostream& operator<<(std::ostream& os, const Array& arr)
+std::ostream& operator<<(std::ostream& os, const IntArray& arr)
 {
+    os << typeid(arr).name() << endl;
     for (int i = 0; i < arr.m_size; i++)
         os << arr.m_ptr[i] << " ";
     os << endl;
@@ -127,7 +134,7 @@ std::ostream& operator<<(std::ostream& os, const Array& arr)
 }
 
 // H: equal-to operator
-bool operator==(const Array& left, const Array& right) {
+bool operator==(const IntArray& left, const IntArray& right) {
     if (left.m_size != right.m_size) {
         return false;
     }
@@ -140,60 +147,92 @@ bool operator==(const Array& left, const Array& right) {
     return true;
 }
 // I: Not-equal-to operator
-bool operator!=(const Array& left, const Array& right) {
+bool operator!=(const IntArray& left, const IntArray& right) {
     return !operator==(left, right);
 }
 
 // J: Pre-increment all elements of the array
-Array& Array::operator ++() {
+IntArray& IntArray::operator ++() {
     for (int i = 0; i < m_size; i++)
         m_ptr[i]++;
     return *this;
 }
 
 // K: Post-increment. Save a dummy array, increment, return the dummy
-Array Array::operator ++(int dummy) {
-    Array temp = *this;
+IntArray IntArray::operator ++(int dummy) {
+    IntArray temp = *this;
     for (int i = 0; i < m_size; i++)
         m_ptr[i] = m_ptr[i] + 1;
     return temp;
 }
 
 // L: Pre-decrement
-Array& Array::operator --() {
+IntArray& IntArray::operator --() {
     for (int i = 0; i < m_size; i++)
         m_ptr[i]--;
     return *this;
 }
 // M: Post-decrement
-Array Array::operator --(int dummy) {
-    Array temp = *this;
+IntArray IntArray::operator --(int dummy) {
+    IntArray temp = *this;
     for (int i = 0; i < m_size; i++)
         m_ptr[i] = m_ptr[i] - 1;
     return temp;
 }
 
+// N: Defining an addition operator for IntArray
+IntArray IntArray::operator+(const IntArray& addend)  {
+    IntArray answer = IntArray(*this);
+    // set limit for the larger array
+    int limit(0);
+    m_size >= addend.m_size ? limit = m_size : limit = addend.m_size;
+    for (int i = 0; i < limit; i++) 
+    {
+        // If beyond the end of the first addend, insert the second addend
+        if (i > m_size) 
+        {
+            answer[i] = addend.m_ptr[i];
+        }
+        // If beyond the end of the second addend, insert the first
+        else if (i > addend.m_ptr[i]) 
+        {
+            answer[i] = m_ptr[i];
+
+        } 
+        // Within the bounds of both, add the elements
+        else answer[i] = m_ptr[i] + addend.m_ptr[i];
+    }
+    return answer;
+
+}
 
 
 int main()
 {
-    cout << "Let's begin!" << endl;
+    cout << "Examples of operator overloading for IntArray" << endl;
 
-    Array a(5);
+    IntArray a(5);
     a[0] = 1;
     a[1] = 2;
     a[2] = 3;
     a[3] = 4;
     a[4] = -1;
 
-    Array b(2);
+    IntArray b(2);
     b[0] = 1;
     b[1] = 2;
+    
     cout << "Display the contents of a and b" << endl;
     cout << "a: " << a << "b: " << b;
 
+    cout << "Display the sum of a and b" << endl;
+    cout << a + b << endl;
+
+    cout << "Did we change a and b?" << endl;
+    cout << a << b << endl;
+
     cout << "Create array c and initialize from a" << endl;
-    Array c = a;
+    IntArray c = a;
     cout << c;
 
     cout << "Set c == b and display c" << endl;
@@ -215,7 +254,7 @@ int main()
 
     cout << "pre and post-increment set of tests\n";
 
-    Array e = c++;
+    IntArray e = c++;
     cout << e++;
     cout << ++e;
     cout << e;
